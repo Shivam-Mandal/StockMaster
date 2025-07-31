e470452bf3dd292cc848c6e912c35dc8938f5f1b
Sourabh Roy <126256243+Sourabh-KRoy@users.noreply.github.com>
2025-07-31 12:25:02 +0530

    login page design

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Login from "./pages/login";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline text-red-600">
-          Hello world!
-        </h1>
-      </div>
+      <Login />
     </>
   );
 }
