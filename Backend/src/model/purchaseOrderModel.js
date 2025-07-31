2a133bf550e6c34f1a46447331aff4276c201a3c
Shivam Mandal <sanchayetamandal83404@gmail.com>
2025-07-31 23:52:51 +0530

    created customerSchema, orderSchema, purchaseSchema, productSchema , purchaseOrderSchema, supplierSchema

---
+++ Backend/src/model/purchaseOrderModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const purchaseOrderSchema = new mongoose.Schema({
+  supplier: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier', required: true },
+  products: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true },
+      costPrice: { type: Number }
+    }
+  ],
+  totalCost: Number,
+  receivedDate: Date,
+  status: {
+    type: String,
+    enum: ['Pending', 'Received'],
+    default: 'Pending'
+  },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const PurchaseOrder = mongoose.model('PurchaseOrder', purchaseOrderSchema);
+export default PurchaseOrder;
