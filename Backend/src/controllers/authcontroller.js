a5e2975ca97497f36e7c2b5e050871fa788c5f6c
rahul-scaleup <rahul@scaleupapp.club>
2025-08-14 11:14:35 +0530

    use me file

--- Backend/src/controllers/authcontroller.js
+++ Backend/src/controllers/authcontroller.js
@@ -76,7 +76,8 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
+      path: '/',
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
